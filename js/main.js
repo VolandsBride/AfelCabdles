fa2eb8d38ab712677dcff61ff854872c651891f9
VolandsBride <volands.bride@gmail.com>
2023-05-11 16:07:14 +0300

    slider + story added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(function () {
-  
+$(document).ready(function(){
+  $('.slider__items').slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    fade: true,
+    dots: false,
+    cssEase: 'linear',
+  });
 });
+
